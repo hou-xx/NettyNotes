669e41c95bb6fb9f1ac924e0a3a01d1029fe56c7
houxx <hou_xiangxiang@126.com>
2019-01-15 11:34:23 +0800

    添加 discard 服务启动器

---
+++ netty-notes-discard/src/main/java/com/hxx/service/DiscardServer.java
@@ -0,0 +1,54 @@
+package com.hxx.service;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * <ul>
+ * <li>功能说明：</li>
+ * <li>作者：tal on 2019/1/15 0015 11:21 </li>
+ * <li>邮箱：houxiangxiang@cibfintech.com</li>
+ * </ul>
+ */
+
+public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // 等待服务器  socket 关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            //优雅地关闭服务
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ netty-notes-discard/src/main/java/com/hxx/service/DiscardStarter.java
@@ -0,0 +1,21 @@
+package com.hxx.service;
+
+/**
+ * <ul>
+ * <li>功能说明：</li>
+ * <li>作者：tal on 2019/1/15 0015 11:25 </li>
+ * <li>邮箱：houxiangxiang@cibfintech.com</li>
+ * </ul>
+ */
+
+public class DiscardStarter {
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new DiscardServer(port).run();
+    }
+}
